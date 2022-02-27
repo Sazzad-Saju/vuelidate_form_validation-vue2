fd25a838eb6decadfdbc5354cdd90d7b3fde0b9c
Sazzad-Saju <sazzadsaju17@gmail.com>
2022-02-27 10:18:05 +0600

    custom validate added

--- src/components/FormValidation.vue
+++ src/components/FormValidation.vue
@@ -5,80 +5,78 @@
         </h3>
         <div class="card-body">
             <form>
-                <div class="form-row">
+                <!-- <div class="form-row"> -->
+                    <!-- name -->
                     <div class="form-group" :class="{ 'form-group--error': $v.name.$error }">
-                        <label>First Name</label>
-                        <input type="text" class="form-control" 
+                      <label>First Name</label>
+                      <input type="text" class="form-control" 
                         v-model.trim="name" 
                         @input="setName($event.target.value)" >
-                        <div class="valid-feedback">
+                      <div class="valid-feedback">
                             Your first name is valid!
-                        </div>
+                      </div>
+                      <div v-if="$v.name.$dirty">
                         <div class="error text-danger mt-3" role="alert" v-if="!$v.name.required">
-                        <small  v-if="!$v.name.required">Field is required</small>
+                          <small  v-if="!$v.name.required">Field is required</small>
                         </div>
                         <div class="error text-danger mt-3" role="alert" v-if="!$v.name.minLength">
-                        <small  v-if="!$v.name.minLength">Name must have at least {{$v.name.$params.minLength.min}} letters.</small>
+                          <small  v-if="!$v.name.minLength">Name must have at least {{$v.name.$params.minLength.min}} letters.</small>
                         </div>
-                        
-
-                        <!-- <div class="invalid-feedback">
-                            <span v-if="!$v.firstname.required">
-                                First name is required
-                            </span>
-                            <span v-if="!$v.firstname.minLength">
-                                First name must have at least {{$v.firstname.$params.minLength.min}} letters.
-                            </span>
-                            <span v-if="!$v.firstname.maxLength">First name must have at most {{$v.firstname.$params.maxLength.max}} letters</span>
-                        </div> -->
+                      </div>
                     </div>
-
-                    <!-- <div>{{firstname}}</div> -->
-
-                    <!-- <div class="form-group">
-                        <label>Last Name</label>
-                        <input type="text" class="form-control" 
-                        v-model.trim="$v.lastname.$model"
-                        :class="{'is-invalid':$v.lastname.$error, 
-                        'is-valid':!$v.lastname.$invalid}">
-                        <div class="valid-feedback">Your last name is valid!</div>
-                        <div class="invalid-feedback">
-                            <span v-if="!$v.lastname.required">
-                                last name is required
-                            </span>
-                            <span v-if="!$v.lastname.minLength">
-                                Last name must have at least {{$v.lastname.$params.minLength.min}} letters.
-                            </span>
-                            <span v-if="!$v.lastname.maxLength">Last name must have at most {{$v.lastname.$params.maxLength.max}} letters</span>
+                    <!-- email -->
+                    <div class="form-group" :class="{ 'form-group--error': $v.email.$error }">
+                      <label>Email</label>
+                      <input type="text" class="form-control" 
+                        v-model.trim="email" 
+                        @input="setEmail($event.target.value)" >
+                      <div class="valid-feedback">
+                            Your email is valid!
+                      </div>
+                      <div v-if="$v.email.$dirty">
+                        <div class="error text-danger mt-3" role="alert" v-if="!$v.email.  required">
+                        <small  v-if="!$v.email.required">Field is required</small>
                         </div>
-                    </div> -->
-                </div>
-                <!-- <div class="form-group">
-                    <label>Age</label>
-                    <input type="number" 
-                    class="form-control" 
-                    v-model.number.lazy="$v.age.$model"
-                    :class="{'is-invalid':$v.age.$error, 
-                    'is-valid':!$v.age.$invalid}">
-                    <div class="valid-feedback">Your age is valid!</div>
-                    <div class="invalid-feedback">
-                        <span v-if="!$v.age.between">Must be between {{$v.age.$params.between.min}} and {{$v.age.$params.between.max}} 
-                        </span>
+                        <div class="error text-danger mt-3" role="alert" v-if="!$v.email.email">
+                          <small>Invalid Email Address </small>
+                        </div>
+                      </div>
                     </div>
-                </div> -->
+                    <!-- phone -->
+                    <div class="form-group" :class="{ 'form-group--error': $v.phone.$error }">
+                      <label>Phone</label>
+                      <input type="text" class="form-control" 
+                        v-model.trim="phone" 
+                        @input="setPhone($event.target.value)" >
+                      <div class="valid-feedback">
+                        Your phone number is valid!
+                      </div>
+                      <div v-if="$v.phone.$dirty">
+                        <div class="error text-danger mt-3" role="alert" v-if="!$v.phone.phoneNumber">
+                          <small>Phone number is invalid</small>
+                        </div>
+                      </div>
+                    </div>
             </form>
         </div>
     </div>
 </template>
 
 <script>
-import { required, minLength, between } from 'vuelidate/lib/validators'
+import { required, minLength, between, email,helpers } from 'vuelidate/lib/validators'
+
+const phoneNumber = helpers.withParams(
+  { type: 'phoneNumber' },
+  (value) => /^(\+)?(88)?01[0-9]{9}$/.test(value)
+)
 
 export default {
   data() {
     return {
       name: '',
-      age: 0
+      age: 0,
+      email: '',
+      phone: '',
     }
   },
   validations: {
@@ -88,7 +86,16 @@ export default {
     },
     age: {
       between: between(20, 30)
-    }
+    },
+    email:{
+      required,
+      email,
+    },
+    phone: {
+      required,
+      phoneNumber,
+    },
+
   },
 
   methods: {
@@ -99,6 +106,14 @@ export default {
     setAge(value) {
       this.age = value
       this.$v.age.$touch()
+    },
+    setEmail(value) {
+      this.email = value
+      this.$v.email.$touch()
+    },
+    setPhone(value) {
+      this.phone = value
+      this.$v.phone.$touch()
     }
   }
 }
